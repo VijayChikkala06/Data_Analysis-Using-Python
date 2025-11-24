266d9e1cc29c32b165f59763e921d6a8542f72df
Vijay Chikkala <vijaykumar161206@gmail.com>
2025-11-24 21:22:11 +0530

    Exp-6

---
+++ Exp-6.py
@@ -0,0 +1,4 @@
+#To write a Python program to get the largest number from the list given
+numbers = [10, 5, 23, 8, 48, 15]
+largest_number = max(numbers)
+print(f"The largest number in the list is: {largest_number}")
