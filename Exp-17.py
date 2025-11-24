ef6f76710691a1e1e1aeb3473b885c4509555c42
Vijay Chikkala <vijaykumar161206@gmail.com>
2025-11-24 21:27:05 +0530

    Exp-17

---
+++ Exp-17.py
@@ -0,0 +1,5 @@
+#To write a Pandas program to create and display a one-dimensional-array-like object containing an array of data using Pandas module.
+import pandas as pd
+import numpy as np
+data_list = pd.Series[10, 20, 30, 40,50])
+print(data_list)
